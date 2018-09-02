abe45250e58a656d6c1bfa1b5625c0a8da1181eb
Vasili Karaev <kratos661@gmail.com>
2018-09-02 17:09:17 +0300

    init(git): Base commit

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+function scrollToMain() {
+  // have to scroll to child here in order to avoid having border in view
+  document.querySelector('main :first-child').scrollIntoView({
+    block: 'start',
+    behavior: 'smooth'});
+}
