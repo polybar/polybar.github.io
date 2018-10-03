8854e869631853bddcb3b23c57acef3646afba88
Vasili Karaev <kratos661@gmail.com>
2018-10-04 00:00:59 +0300

    refactor(js): Adjust formatting

--- js/scroll.js
+++ js/scroll.js
@@ -2,5 +2,6 @@ function scrollToMain() {
   // have to scroll to child here in order to avoid having border in view
   document.querySelector('main :first-child').scrollIntoView({
     block: 'start',
-    behavior: 'smooth'});
+    behavior: 'smooth'
+  });
 }
